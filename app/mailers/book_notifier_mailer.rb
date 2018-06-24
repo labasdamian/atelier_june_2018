e0b3526f77639c07ec4c850a4191905b54c5ccf9
Damian Łabas <damian.labas@codibly.com>
2018-06-24 14:00:12 +0200

    fix book mailer

--- app/mailers/book_notifier_mailer.rb
+++ app/mailers/book_notifier_mailer.rb
@@ -3,16 +3,20 @@ class BookNotifierMailer < ApplicationMailer
   def book_return_remind(book)
     @book = book
     @reservation = book.reservations.find_by(status: 'TAKEN')
-    @borrower = @reservation.user
+    @borrower = @reservation.try(:user)
+
+    return if @borrower.blank?
 
     mail(to: @borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}")
   end
 
   def book_reserved_return(book)
     @book = book
-    @reservation = book.reservations.find_by(status: 'RESERVED')
-    @borrower = @reservation.user
+    @reservation = book.reservations.find_by(status: 'TAKEN')
+    @reserver = book.reservations.where(status: 'RESERVED').first.try(:user);
 
-    mail(to: @borrower.email, subject: "Ksiażka #{@book.title} będzie dostępna za niedługa")
+    return if @reserver.blank? || @reservation.blank?
+
+    mail(to: @reserver.email, subject: "Upływa termin zwrotu książki #{@book.title}")
   end
 end
